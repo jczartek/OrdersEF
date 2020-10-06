058569e88d9ae2c29342c184c581efef21821be5
Jakub Czartek <kuba@linux.pl>
2020-10-06 18:25:04 +0200

    first migration

---
+++ DataLayer/Migrations/20201006161355_Initial Migration.cs
@@ -0,0 +1,403 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataLayer.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "Production");
+
+            migrationBuilder.EnsureSchema(
+                name: "Sales");
+
+            migrationBuilder.EnsureSchema(
+                name: "HR");
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                schema: "HR",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LastName = table.Column<string>(maxLength: 20, nullable: false),
+                    FirstName = table.Column<string>(maxLength: 10, nullable: false),
+                    Title = table.Column<string>(maxLength: 30, nullable: false),
+                    TitleOfCourtesy = table.Column<string>(maxLength: 25, nullable: false),
+                    BirthDate = table.Column<DateTime>(nullable: false),
+                    HireDate = table.Column<DateTime>(nullable: false),
+                    Address = table.Column<string>(maxLength: 60, nullable: false),
+                    City = table.Column<string>(maxLength: 15, nullable: false),
+                    Region = table.Column<string>(maxLength: 15, nullable: true),
+                    PostalCode = table.Column<string>(maxLength: 10, nullable: true),
+                    Country = table.Column<string>(maxLength: 15, nullable: false),
+                    Phone = table.Column<string>(maxLength: 24, nullable: false),
+                    MgrId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmployeeId);
+                    table.CheckConstraint("CHK_birthdate", "([birthdate] <= CONVERT([date], sysdatetime()))");
+                    table.ForeignKey(
+                        name: "FK_Employees_Employees_MgrId",
+                        column: x => x.MgrId,
+                        principalSchema: "HR",
+                        principalTable: "Employees",
+                        principalColumn: "EmployeeId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                schema: "Production",
+                columns: table => new
+                {
+                    CategoryId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryName = table.Column<string>(maxLength: 15, nullable: false),
+                    Description = table.Column<string>(maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategoryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Suppliers",
+                schema: "Production",
+                columns: table => new
+                {
+                    SupplierId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CompanyName = table.Column<string>(maxLength: 40, nullable: false),
+                    ContactName = table.Column<string>(maxLength: 30, nullable: false),
+                    ContactTitle = table.Column<string>(maxLength: 30, nullable: false),
+                    Address = table.Column<string>(maxLength: 60, nullable: false),
+                    City = table.Column<string>(maxLength: 15, nullable: false),
+                    Region = table.Column<string>(maxLength: 15, nullable: true),
+                    PostalCode = table.Column<string>(maxLength: 10, nullable: true),
+                    Country = table.Column<string>(maxLength: 15, nullable: false),
+                    Phone = table.Column<string>(maxLength: 24, nullable: false),
+                    Fax = table.Column<string>(maxLength: 24, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Suppliers", x => x.SupplierId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                schema: "Sales",
+                columns: table => new
+                {
+                    CustomerId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CompanyName = table.Column<string>(maxLength: 40, nullable: false),
+                    ContactName = table.Column<string>(maxLength: 30, nullable: false),
+                    ContactTitle = table.Column<string>(maxLength: 30, nullable: false),
+                    Address = table.Column<string>(maxLength: 60, nullable: false),
+                    City = table.Column<string>(maxLength: 15, nullable: false),
+                    Region = table.Column<string>(maxLength: 15, nullable: true),
+                    PostalCode = table.Column<string>(maxLength: 10, nullable: true),
+                    Country = table.Column<string>(maxLength: 15, nullable: false),
+                    Phone = table.Column<string>(maxLength: 24, nullable: false),
+                    Fax = table.Column<string>(maxLength: 24, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.CustomerId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Shippers",
+                schema: "Sales",
+                columns: table => new
+                {
+                    ShipperId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CompanyName = table.Column<string>(maxLength: 40, nullable: false),
+                    Phone = table.Column<string>(maxLength: 24, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Shippers", x => x.ShipperId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                schema: "Production",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductName = table.Column<string>(maxLength: 40, nullable: false),
+                    UnitPrice = table.Column<decimal>(type: "Money", nullable: false),
+                    Discontinued = table.Column<bool>(nullable: false),
+                    SupplierId = table.Column<int>(nullable: false),
+                    CategoryId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ProductId);
+                    table.CheckConstraint("CHK_Product_unitprice", "([unitprice] >= (0))");
+                    table.ForeignKey(
+                        name: "FK_Products_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalSchema: "Production",
+                        principalTable: "Categories",
+                        principalColumn: "CategoryId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Products_Suppliers_SupplierId",
+                        column: x => x.SupplierId,
+                        principalSchema: "Production",
+                        principalTable: "Suppliers",
+                        principalColumn: "SupplierId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                schema: "Sales",
+                columns: table => new
+                {
+                    OrderId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrderDate = table.Column<DateTime>(nullable: false),
+                    RequiredDate = table.Column<DateTime>(nullable: false),
+                    ShippedDate = table.Column<DateTime>(nullable: true),
+                    Freight = table.Column<decimal>(type: "money", nullable: false),
+                    ShipName = table.Column<string>(maxLength: 40, nullable: false),
+                    ShipAddress = table.Column<string>(maxLength: 60, nullable: false),
+                    ShipCity = table.Column<string>(maxLength: 15, nullable: false),
+                    ShipRegion = table.Column<string>(maxLength: 15, nullable: true),
+                    ShipPostalCode = table.Column<string>(maxLength: 10, nullable: true),
+                    ShipCountry = table.Column<string>(maxLength: 15, nullable: false),
+                    CustomerId = table.Column<int>(nullable: false),
+                    EmployeeId = table.Column<int>(nullable: false),
+                    ShipperId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.OrderId);
+                    table.ForeignKey(
+                        name: "FK_Orders_Customers_CustomerId",
+                        column: x => x.CustomerId,
+                        principalSchema: "Sales",
+                        principalTable: "Customers",
+                        principalColumn: "CustomerId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Orders_Employees_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalSchema: "HR",
+                        principalTable: "Employees",
+                        principalColumn: "EmployeeId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Orders_Shippers_ShipperId",
+                        column: x => x.ShipperId,
+                        principalSchema: "Sales",
+                        principalTable: "Shippers",
+                        principalColumn: "ShipperId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderDetails",
+                schema: "Sales",
+                columns: table => new
+                {
+                    OrderId = table.Column<int>(nullable: false),
+                    ProductId = table.Column<int>(nullable: false),
+                    UnitPrice = table.Column<decimal>(type: "money", nullable: false),
+                    Qty = table.Column<short>(type: "smallint", nullable: false),
+                    Discount = table.Column<decimal>(type: "numeric(4,3)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderDetails", x => new { x.OrderId, x.ProductId });
+                    table.CheckConstraint("CHK_discount", "([Discount] >= (0) AND [Discount] <= (1))");
+                    table.CheckConstraint("CHK_qty", "([Qty]>(0))");
+                    table.CheckConstraint("CHK_unitprice", "([UnitPrice]>=(0))");
+                    table.ForeignKey(
+                        name: "FK_OrderDetails_Orders_OrderId",
+                        column: x => x.OrderId,
+                        principalSchema: "Sales",
+                        principalTable: "Orders",
+                        principalColumn: "OrderId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderDetails_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalSchema: "Production",
+                        principalTable: "Products",
+                        principalColumn: "ProductId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "idx_lastname",
+                schema: "HR",
+                table: "Employees",
+                column: "LastName");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_MgrId",
+                schema: "HR",
+                table: "Employees",
+                column: "MgrId");
+
+            migrationBuilder.CreateIndex(
+                name: "idx_postalcode",
+                schema: "HR",
+                table: "Employees",
+                column: "PostalCode");
+
+            migrationBuilder.CreateIndex(
+                name: "idx_categoryname",
+                schema: "Production",
+                table: "Categories",
+                column: "CategoryName");
+
+            migrationBuilder.CreateIndex(
+                name: "idx_categoryid",
+                schema: "Production",
+                table: "Products",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "idx_productname",
+                schema: "Production",
+                table: "Products",
+                column: "ProductName");
+
+            migrationBuilder.CreateIndex(
+                name: "idx_supplierid",
+                schema: "Production",
+                table: "Products",
+                column: "SupplierId");
+
+            migrationBuilder.CreateIndex(
+                name: "idx_companyname",
+                schema: "Production",
+                table: "Suppliers",
+                column: "CompanyName");
+
+            migrationBuilder.CreateIndex(
+                name: "idx_postalcode",
+                schema: "Production",
+                table: "Suppliers",
+                column: "PostalCode");
+
+            migrationBuilder.CreateIndex(
+                name: "idx_city",
+                schema: "Sales",
+                table: "Customers",
+                column: "City");
+
+            migrationBuilder.CreateIndex(
+                name: "idx_companyname",
+                schema: "Sales",
+                table: "Customers",
+                column: "CompanyName");
+
+            migrationBuilder.CreateIndex(
+                name: "idx_postalcode",
+                schema: "Sales",
+                table: "Customers",
+                column: "PostalCode");
+
+            migrationBuilder.CreateIndex(
+                name: "idx_region",
+                schema: "Sales",
+                table: "Customers",
+                column: "Region");
+
+            migrationBuilder.CreateIndex(
+                name: "idx_orderid",
+                schema: "Sales",
+                table: "OrderDetails",
+                column: "OrderId");
+
+            migrationBuilder.CreateIndex(
+                name: "idx_productid",
+                schema: "Sales",
+                table: "OrderDetails",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "idx_customerid",
+                schema: "Sales",
+                table: "Orders",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "idx_employeeid",
+                schema: "Sales",
+                table: "Orders",
+                column: "EmployeeId");
+
+            migrationBuilder.CreateIndex(
+                name: "idx_orderdate",
+                schema: "Sales",
+                table: "Orders",
+                column: "OrderDate");
+
+            migrationBuilder.CreateIndex(
+                name: "idx_shippostalcode",
+                schema: "Sales",
+                table: "Orders",
+                column: "ShipPostalCode");
+
+            migrationBuilder.CreateIndex(
+                name: "idx_shippeddate",
+                schema: "Sales",
+                table: "Orders",
+                column: "ShippedDate");
+
+            migrationBuilder.CreateIndex(
+                name: "idx_shipperid",
+                schema: "Sales",
+                table: "Orders",
+                column: "ShipperId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderDetails",
+                schema: "Sales");
+
+            migrationBuilder.DropTable(
+                name: "Orders",
+                schema: "Sales");
+
+            migrationBuilder.DropTable(
+                name: "Products",
+                schema: "Production");
+
+            migrationBuilder.DropTable(
+                name: "Customers",
+                schema: "Sales");
+
+            migrationBuilder.DropTable(
+                name: "Employees",
+                schema: "HR");
+
+            migrationBuilder.DropTable(
+                name: "Shippers",
+                schema: "Sales");
+
+            migrationBuilder.DropTable(
+                name: "Categories",
+                schema: "Production");
+
+            migrationBuilder.DropTable(
+                name: "Suppliers",
+                schema: "Production");
+        }
+    }
+}
