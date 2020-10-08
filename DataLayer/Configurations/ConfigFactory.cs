cd5755cec6b430994a37b5c3668e9d20684bf91b
Jakub Czartek <kuba@linux.pl>
2020-10-08 11:29:35 +0200

    check if object implements interface

--- DataLayer/Configurations/ConfigFactory.cs
+++ DataLayer/Configurations/ConfigFactory.cs
@@ -2,10 +2,11 @@
 using System;
 using System.Collections.Generic;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 
 namespace DataLayer.Configurations
 {
-    public class ConfigFactory
+    public static class ConfigFactory
     {
         public static IEntityTypeConfiguration<TEntity> ConfigFor<TEntity>()
             where TEntity : class, new()
@@ -16,7 +17,15 @@ namespace DataLayer.Configurations
             if (!TryFindType(nameTypeContigClass, out Type type))
                 throw new ArgumentException($"{nameTypeContigClass} doesn't exist");
 
+            if (!type.Implements<IEntityTypeConfiguration<TEntity>>())
+                throw new ArgumentException($"{ typeof(IEntityTypeConfiguration<TEntity>).Name} is not implemented by {nameTypeContigClass}");
+
             return (IEntityTypeConfiguration<TEntity>) type.Assembly.CreateInstance(nameTypeContigClass);
+        }
+
+        private static bool Implements<I>(this Type source) where I : class
+        {
+            return typeof(I).IsInterface && typeof(I).IsAssignableFrom(source);
         }
 
         private static readonly Dictionary<string, Type> typeCache = new Dictionary<string, Type>();
